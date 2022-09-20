fca33ee7a2ff87e6e6188e86f79e0379fbed3884
Pedro Silva <pedro.silva@cbyk.com.br>
2022-09-20 13:55:57 -0300

    feat: animations

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,16 +1,17 @@
 import tw from 'twin.macro'
 import styled from 'styled-components'
 import { AppButton, AppContainer } from '../css/ts/components'
+import { fadeIn } from '../animation'
 
 export const Container = styled(AppContainer)`
     ${tw`h-screen flex flex-col justify-center items-center`}
 `
 
-export const Title = styled.h1`
+export const Title = styled.h1.attrs(fadeIn.down)`
     ${tw`text-lg`}
 `
 
-export const Image = styled.img`
+export const Image = styled.img.attrs(fadeIn.default)`
     ${tw`mt-8`}
 `
 
@@ -18,7 +19,7 @@ export const Content = styled.article`
     ${tw`mt-8`}
 `
 
-export const Button = styled(AppButton)`
+export const Button = styled(AppButton).attrs(fadeIn.up)`
     ${tw`min-w-[150px]`}
 
     &:first-of-type {
